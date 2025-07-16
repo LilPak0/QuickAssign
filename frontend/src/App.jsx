afd2a19b25e7c6380f5ede29f74bc8434786e72f
Wajiha3 <wajihaaziz195@gmail.com>
2025-07-16 21:54:01 +0100

    "ongoing"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import DashBoard from './pages/Dashboard';
 import Ongoing from './pages/Ongoing';
+import Finish from './pages/Finish';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DashBoard />} />
         <Route path="/ongoing" element={<Ongoing></Ongoing>} />
+        <Route path="/finished" element={<Finish />} />
         {/* Add more routes as needed */}
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   
   return (
     <nav className="bg-white shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo on the left */}
           <div className="flex items-center">
